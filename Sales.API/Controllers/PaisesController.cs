e8130e1bf19f5d86ebff6cbded8fb3235f23c207
ValentinVillegas <j.valentin1@hotmail.com>
2023-06-20 16:23:42 -0600

    Se comenzó la creación de controladores para paises

---
+++ Sales.API/Controllers/PaisesController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Sales.API.Data;
+using Sales.Shared.Entidades;
+
+namespace Sales.API.Controllers
+{
+    [ApiController]
+    [Route("/api/paises")]
+    public class PaisesController:ControllerBase
+    {
+        private readonly DataContext _context;
+
+        public PaisesController(DataContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAsync()
+        {
+            return Ok(await _context.Paises.ToListAsync());
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> PostAsync(Pais pais)
+        {
+            _context.Add(pais);
+            await _context.SaveChangesAsync();
+            return Ok(pais);
+        }
+    }
+}
