d386c51e05bfb985f866bb4ca2c19d4984d7cd30
ValentinVillegas <j.valentin1@hotmail.com>
2023-06-19 18:39:43 -0600

    Se agregó DataContext

---
+++ Sales.API/Data/DataContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Sales.Shared.Entidades;
+
+namespace Sales.API.Data
+{
+    public class DataContext:DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> optiones) : base(optiones)
+        {
+
+        }
+
+        public DbSet<Pais> Paises { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Pais>().HasIndex(x => x.Nombre).IsUnique();
+        }
+    }
+}
---
+++ Sales.API/Migrations/20230620003340_InicialDB.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Sales.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class InicialDB : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Paises",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nombre = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Paises", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Paises_Nombre",
+                table: "Paises",
+                column: "Nombre",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Paises");
+        }
+    }
+}
