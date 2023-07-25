4966ef6c58465d8c2e6eae1e255a716d80d5e78e
ValentinVillegas <j.valentin1@hotmail.com>
2023-07-25 12:53:07 -0600

    Se insertaron Paises, Estados y Municipios provenientes de una Api externa

--- Sales.API/Data/SeedDb.cs
+++ Sales.API/Data/SeedDb.cs
@@ -1,123 +1,208 @@
-﻿using Sales.Shared.Entidades;
+﻿using Microsoft.EntityFrameworkCore;
+using Sales.API.Services;
+using Sales.Shared.Entidades;
+using Sales.Shared.Responses;
+using System.Linq.Expressions;
 
 namespace Sales.API.Data
 {
     public class SeedDb
     {
         private readonly DataContext _context;
+        private readonly IApiService _apiService;
 
-        public SeedDb(DataContext context)
+        public SeedDb(DataContext context, IApiService apiService)
         {
             _context = context;
+            _apiService = apiService;
         }
 
         public async Task SeedAsync()
         {
             await _context.Database.EnsureCreatedAsync();
-            await CheckPaisesAsync();
+            //await CheckPaisesAsync();
         }
 
         private async Task CheckPaisesAsync()
         {
             if (!_context.Paises.Any())
             {
-                _context.Paises.Add(new Pais 
+                /*
                 {
-                    Nombre = "Estados Unidos",
-                    Estados = new List<Estado>()
-                    {
-                        new Estado()
+                    _context.Paises.Add(new Pais
                         {
-                            Nombre = "Florida",
-                            Municipios = new List<Municipio>()
-                            {
-                                new Municipio{Nombre = "Orlando"},
-                                new Municipio{Nombre = "Miami"},
-                                new Municipio{Nombre = "Tampa"},
-                                new Municipio{Nombre = "Fort Lauderdale"},
-                                new Municipio{Nombre = "Key West"}
-                            }
-                        },
-                        new Estado()
-                        {
-                            Nombre = "Texas",
-                            Municipios = new List<Municipio>()
-                            {
-                                new Municipio{Nombre = "Houston"},
-                                new Municipio{Nombre = "San Antonio"},
-                                new Municipio{Nombre = "Dallas"},
-                                new Municipio{Nombre = "Austin"},
-                                new Municipio{Nombre = "El Paso"}
-                            }
+                            Nombre = "Estados Unidos",
+                            Estados = new List<Estado>()
+                                {
+                                    new Estado()
+                                    {
+                                        Nombre = "Florida",
+                                        Municipios = new List<Municipio>()
+                                        {
+                                            new Municipio{Nombre = "Orlando"},
+                                            new Municipio{Nombre = "Miami"},
+                                            new Municipio{Nombre = "Tampa"},
+                                            new Municipio{Nombre = "Fort Lauderdale"},
+                                            new Municipio{Nombre = "Key West"}
+                                        }
+                                    },
+                                    new Estado()
+                                    {
+                                        Nombre = "Texas",
+                                        Municipios = new List<Municipio>()
+                                        {
+                                            new Municipio{Nombre = "Houston"},
+                                            new Municipio{Nombre = "San Antonio"},
+                                            new Municipio{Nombre = "Dallas"},
+                                            new Municipio{Nombre = "Austin"},
+                                            new Municipio{Nombre = "El Paso"}
+                                        }
+                                    }
+                                }
                         }
-                    }
-                });
+                    );
 
-                _context.Paises.Add(new Pais
-                {
-                    Nombre = "México",
-                    Estados = new List<Estado>()
-                    {
-                        new Estado()
+                    _context.Paises.Add(new Pais
                         {
-                            Nombre = "Nuevo León",
-                            Municipios = new List<Municipio>()
-                            {
-                                new Municipio{Nombre = "Monterrey"},
-                                new Municipio{Nombre = "Guadalupe"},
-                                new Municipio{Nombre = "Escobedo"},
-                                new Municipio{Nombre = "San Nicolas"},
-                                new Municipio{Nombre = "Cadereyta"}
-                            }
-                        },
-                        new Estado()
-                        {
-                            Nombre = "Jalisco",
-                            Municipios = new List<Municipio>()
-                            {
-                                new Municipio{Nombre = "Jesús María"},
-                                new Municipio{Nombre = "Ocotlán"},
-                                new Municipio{Nombre = "Puerto Vallarta"},
-                                new Municipio{Nombre = "Tequila"},
-                                new Municipio{Nombre = "Tonalá"}
-                            }
+                            Nombre = "México",
+                            Estados = new List<Estado>()
+                                {
+                                    new Estado()
+                                    {
+                                        Nombre = "Nuevo León",
+                                        Municipios = new List<Municipio>()
+                                        {
+                                            new Municipio{Nombre = "Monterrey"},
+                                            new Municipio{Nombre = "Guadalupe"},
+                                            new Municipio{Nombre = "Escobedo"},
+                                            new Municipio{Nombre = "San Nicolas"},
+                                            new Municipio{Nombre = "Cadereyta"}
+                                        }
+                                    },
+                                    new Estado()
+                                    {
+                                        Nombre = "Jalisco",
+                                        Municipios = new List<Municipio>()
+                                        {
+                                            new Municipio{Nombre = "Jesús María"},
+                                            new Municipio{Nombre = "Ocotlán"},
+                                            new Municipio{Nombre = "Puerto Vallarta"},
+                                            new Municipio{Nombre = "Tequila"},
+                                            new Municipio{Nombre = "Tonalá"}
+                                        }
+                                    }
+                                }
                         }
-                    }
-                });
+                    );
 
-                _context.Paises.Add(new Pais
-                {
-                    Nombre = "Colombia",
-                    Estados = new List<Estado>()
-                    {
-                        new Estado()
+                    _context.Paises.Add(new Pais
                         {
-                            Nombre = "Antioquia",
-                            Municipios = new List<Municipio>()
-                            {
-                                new Municipio{Nombre = "Medellín"},
-                                new Municipio{Nombre = "Itagüí"},
-                                new Municipio{Nombre = "Envigado"},
-                                new Municipio{Nombre = "Bello"},
-                                new Municipio{Nombre = "Rionegro"}
-                            }
-                        },
-                        new Estado()
-                        {
-                            Nombre = "Bogota",
-                            Municipios = new List<Municipio>()
-                            {
-                                new Municipio{Nombre = "Usuaquen"},
-                                new Municipio{Nombre = "Champinero"},
-                                new Municipio{Nombre = "Santa Fe"},
-                                new Municipio{Nombre = "Useme"},
-                                new Municipio{Nombre = "Bosa"}
-                            }
+                            Nombre = "Colombia",
+                            Estados = new List<Estado>()
+                                {
+                                    new Estado()
+                                    {
+                                        Nombre = "Antioquia",
+                                        Municipios = new List<Municipio>()
+                                        {
+                                            new Municipio{Nombre = "Medellín"},
+                                            new Municipio{Nombre = "Itagüí"},
+                                            new Municipio{Nombre = "Envigado"},
+                                            new Municipio{Nombre = "Bello"},
+                                            new Municipio{Nombre = "Rionegro"}
+                                        }
+                                    },
+                                    new Estado()
+                                    {
+                                        Nombre = "Bogota",
+                                        Municipios = new List<Municipio>()
+                                        {
+                                            new Municipio{Nombre = "Usuaquen"},
+                                            new Municipio{Nombre = "Champinero"},
+                                            new Municipio{Nombre = "Santa Fe"},
+                                            new Municipio{Nombre = "Useme"},
+                                            new Municipio{Nombre = "Bosa"}
+                                        }
+                                    }
+                                }
                         }
-                    }
-                });
-
+                    );
+                }
                 await _context.SaveChangesAsync();
+                */
+
+                Response responsePaises = await _apiService.GetListAsync<PaisResponse>("/v1", "/countries");
+                if (responsePaises.IsSucces)
+                {
+                    List<PaisResponse> paises = (List<PaisResponse>)responsePaises.Result!;
+
+                    foreach (PaisResponse paisResponse in paises)
+                    {
+                        Pais pais = await _context.Paises!.FirstOrDefaultAsync(c => c.Nombre == paisResponse.Name!)!;
+
+                        if(pais == null)
+                        {
+                            pais = new()
+                            {
+                                Nombre = paisResponse.Name!,
+                                Estados = new List<Estado>()
+                            };
+
+                            Response responseEstados = await _apiService.GetListAsync<EstadoResponse>("/v1", $"/countries/{paisResponse.Iso2}/states");
+
+                            if (responseEstados.IsSucces)
+                            {
+                                List<EstadoResponse> estados = (List<EstadoResponse>)responseEstados.Result!;
+
+                                foreach(EstadoResponse estadoResponse in estados)
+                                {
+                                    Estado estado = pais.Estados!.FirstOrDefault(e => e.Nombre == estadoResponse.Name!)!;
+                                    if(estado is null)
+                                    {
+                                        estado = new()
+                                        {
+                                            Nombre = estadoResponse.Name!,
+                                            Municipios = new List<Municipio>()
+                                        };
+
+                                        Response responseMunicipios = await _apiService.GetListAsync<MunicipioResponse>("/v1", $"/countries/{paisResponse.Iso2}/states/{estadoResponse.Iso2}/cities");
+
+                                        if (responseMunicipios.IsSucces)
+                                        {
+                                            List<MunicipioResponse> municipios = (List<MunicipioResponse>)responseMunicipios.Result!;
+                                            
+                                            foreach(MunicipioResponse municipioResponse in municipios)
+                                            {
+                                                if (municipioResponse.Name == "Mosfellsbær" || municipioResponse.Name == "Șăulița")
+                                                {
+                                                    continue;
+                                                }
+
+                                                Municipio municipio = estado.Municipios!.FirstOrDefault(m => m.Nombre == municipioResponse.Name!)!;
+                                                if (municipio is null)
+                                                {
+                                                    estado.Municipios.Add(new Municipio() { Nombre = municipioResponse.Name! });
+                                                }
+                                            }
+                                        }
+
+                                        if (estado.CantidadMunicipios > 0)
+                                        {
+                                            pais.Estados.Add(estado);
+                                        }
+
+                                    }
+                                }
+                            }
+
+                            if(pais.CantidadEstados > 0)
+                            {
+                                _context.Paises.Add(pais);
+                                await _context.SaveChangesAsync();
+                            }
+                        }
+                    }
+                }
             }
         }
     }
---
+++ Sales.API/Services/ApiService.cs
@@ -0,0 +1,60 @@
+﻿using Newtonsoft.Json;
+using Sales.Shared.Responses;
+using System.Text.Json.Serialization;
+
+namespace Sales.API.Services
+{
+    public class ApiService : IApiService
+    {
+        private readonly string _urlBase;
+        private readonly string _tokenName;
+        private readonly string _tokenValue;
+
+        public ApiService(IConfiguration configuration)
+        {
+            _urlBase = configuration["CountriesAPI:urlBase"]!;
+            _tokenName = configuration["CountriesAPI:tokenName"]!;
+            _tokenValue = configuration["CountriesAPI:tokenValue"]!;
+        }
+
+        public async Task<Response> GetListAsync<T>(string servicePrefix, string controller)
+        {
+            try
+            {
+                HttpClient client = new HttpClient { 
+                    BaseAddress = new Uri(_urlBase),
+                };
+
+                client.DefaultRequestHeaders.Add(_tokenName, _tokenValue);
+                string url = $"{servicePrefix}{controller}";
+                HttpResponseMessage response = await client.GetAsync(url);
+                string result = await response.Content.ReadAsStringAsync();
+
+                if (!response.IsSuccessStatusCode)
+                {
+                    return new Response
+                    {
+                        IsSucces = false,
+                        Message = result
+                    };
+                }
+
+                List<T> list = JsonConvert.DeserializeObject<List<T>>(result)!;
+
+                return new Response
+                {
+                    IsSucces = true,
+                    Result = list
+                };
+            }
+            catch (Exception ex)
+            {
+                return new Response
+                {
+                    IsSucces = false,
+                    Message = ex.Message
+                };
+            }
+        }
+    }
+}
