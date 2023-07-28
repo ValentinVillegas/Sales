aa77c933913ab990bab33654ce0d5182d74d367d
ValentinVillegas <j.valentin1@hotmail.com>
2023-07-28 11:24:20 -0600

    Se agregó seguridad en el proyecto web

---
+++ Sales.WEB/Auth/AuthenticationProviderTest.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Components.Authorization;
+using System.Security.Claims;
+
+namespace Sales.WEB.Auth
+{
+    public class AuthenticationProviderTest: AuthenticationStateProvider
+    {
+        public async override Task<AuthenticationState> GetAuthenticationStateAsync()
+        {
+            var anonimous = new ClaimsIdentity();
+            var pruebaUser = new ClaimsIdentity(new List<Claim> {
+                new Claim("FirstName", "Valentin"),
+                new Claim("LastName", "Villegas"),
+                new Claim(ClaimTypes.Name, "valentinvillegas22@yopmail.com"),
+                new Claim(ClaimTypes.Role, "Admin")
+            }, authenticationType: "test") ;
+            return await Task.FromResult(new AuthenticationState(new ClaimsPrincipal(pruebaUser)));
+        }
+    }
+}
