8cc3592c8f4f575acccde9765bc713231c3e5e1b
ValentinVillegas <j.valentin1@hotmail.com>
2023-08-10 12:10:51 -0600

    Se agregó paginación a controlador de categorias

--- Sales.API/Controllers/CategoriasController.cs
+++ Sales.API/Controllers/CategoriasController.cs
@@ -1,11 +1,16 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Sales.API.Data;
+using Sales.API.Helpers;
+using Sales.Shared.DTOs;
 using Sales.Shared.Entidades;
 
 namespace Sales.API.Controllers
 {
     [ApiController]
+    [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
     [Route("/api/categorias")]
     public class CategoriasController : ControllerBase
     {
@@ -17,10 +22,33 @@ namespace Sales.API.Controllers
         }
 
         [HttpGet]
-        public async Task<ActionResult> GetAsync()
+        public async Task<ActionResult> GetAsync([FromQuery] PaginationDTO paginacion)
         {
-            var queryable = _context.Categorias;
-            return Ok(await queryable.OrderBy(c => c.Nombre).ToListAsync());
+            var queryable = _context.Categorias.AsQueryable();
+
+            if (!string.IsNullOrWhiteSpace(paginacion.Filter)) queryable = queryable.Where(x => x.Nombre.ToLower().Contains(paginacion.Filter.ToLower()));
+
+            return Ok(await queryable.OrderBy(c => c.Nombre).Paginate(paginacion).ToListAsync());
+        }
+
+        [HttpGet("{id:int}")]
+        public async Task<ActionResult> GetAsync(int id)
+        {
+            var categoria = await _context.Categorias.FirstOrDefaultAsync(c => c.Id == id);
+
+            if(categoria is null) return NotFound();
+
+            return Ok(categoria);
+        }
+
+        [HttpGet("totalPages")]
+        public async Task<ActionResult> GetPaginas([FromQuery] PaginationDTO paginacion)
+        {
+            var queryable = _context.Categorias.AsQueryable();
+            if (!string.IsNullOrWhiteSpace(paginacion.Filter)) queryable = queryable.Where(c => c.Nombre.ToLower().Contains(paginacion.Filter.ToLower()));
+            double cantidad = await queryable.CountAsync();
+            double totalPaginas = Math.Ceiling(cantidad / paginacion.RecordsNumber);
+            return Ok(totalPaginas);
         }
 
         [HttpPost]
@@ -34,7 +62,7 @@ namespace Sales.API.Controllers
             }
             catch (DbUpdateException dbUpdateException)
             {
-                if (dbUpdateException.InnerException.Message.Contains("duplicate"))
+                if (dbUpdateException.InnerException!.Message.Contains("duplicate"))
                 {
                     return BadRequest("Ya existe un país con el mismo nombre");
                 }
@@ -59,7 +87,7 @@ namespace Sales.API.Controllers
             }
             catch (DbUpdateException dbUpdateException)
             {
-                if (dbUpdateException.InnerException.Message.Contains("duplicate"))
+                if (dbUpdateException.InnerException!.Message.Contains("duplicate"))
                 {
                     return BadRequest("Ya existe un país con el mismo nombre");
                 }
