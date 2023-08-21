570a2cb53202d4f2b1f97992440aac3043b20207
ValentinVillegas <j.valentin1@hotmail.com>
2023-08-21 16:41:13 -0600

    Se agregó el index de las ordenes generadas

--- Sales.API/Controllers/OrdenesController.cs
+++ Sales.API/Controllers/OrdenesController.cs
@@ -1,8 +1,11 @@
 ﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Sales.API.Data;
 using Sales.API.Helpers;
 using Sales.Shared.DTOs;
+using Sales.Shared.Enums;
 
 namespace Sales.API.Controllers
 {
@@ -12,14 +15,53 @@ namespace Sales.API.Controllers
     public class OrdenesController:ControllerBase
     {
         private readonly IOrdenHelper _ordenHelper;
+        private readonly DataContext _context;
+        private readonly IUserHelper _userHelper;
 
-        public OrdenesController(IOrdenHelper ordenHelper)
+        public OrdenesController(IOrdenHelper ordenHelper, DataContext context, IUserHelper userHelper)
         {
             _ordenHelper = ordenHelper;
+            _context = context;
+            _userHelper = userHelper;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAsync([FromQuery] PaginationDTO paginacion)
+        {
+            var usuario = await _context.Users.FirstOrDefaultAsync(x => x.Email == User.Identity!.Name);
+
+            if (usuario is null) return BadRequest("Usuario no válido");
+
+            var queryable = _context.Ordenes.Include(o => o.Usuario).Include(o => o.OrdenDetalles!).ThenInclude(o => o.Producto).AsQueryable();
+
+            var esAdministrador = await _userHelper.IsUserInRoleAsync(usuario, UserType.Admin.ToString());
+
+            if (!esAdministrador) queryable = queryable.Where(o => o.Usuario!.Email == User.Identity!.Name);
+
+            return Ok(await queryable.OrderByDescending(x => x.Fecha).Paginate(paginacion).ToListAsync());
+        }
+
+        [HttpGet("totalPages")]
+        public async Task<IActionResult> GetPaginas([FromQuery] PaginationDTO paginacion)
+        {
+            var usuario = await _context.Users.FirstOrDefaultAsync(x => x.Email == User.Identity!.Name);
+
+            if (usuario is null) return BadRequest("Usuaio no válido");
+
+            var queryable = _context.Ordenes.AsQueryable();
+
+            var esAdministrador = await _userHelper.IsUserInRoleAsync(usuario, UserType.Admin.ToString());
+
+            if (!esAdministrador) queryable = queryable.Where(x => x.Usuario!.Email == User.Identity!.Name);
+
+            double cantidad = await queryable.CountAsync();
+            double totalPaginas = Math.Ceiling(cantidad / paginacion.RecordsNumber);
+
+            return Ok(totalPaginas);
         }
 
         [HttpPost]
-        public async Task<IActionResult> Post(OrdenDTO ordenDTO)
+        public async Task<IActionResult> PostAsync(OrdenDTO ordenDTO)
         {
             var response = await _ordenHelper.ProcesarOrdenAsync(User.Identity!.Name!, ordenDTO.Comentarios);
 
